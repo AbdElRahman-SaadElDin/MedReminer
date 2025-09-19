b13608278d61e92c93809d3e78a071b6a0c2f9a3
amr <amrashraf314@gmail.com>
2025-09-19 06:54:44 +0300

    edit any type

--- src/app/components/MedicalProfile.tsx
+++ src/app/components/MedicalProfile.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // app/components/MedicalProfileClient.tsx
 "use client";
 
--- src/components/EditPatientModal.tsx
+++ src/components/EditPatientModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ReactNode, useId, useState } from "react";
 
 import { Button } from "@/components/ui/button";
